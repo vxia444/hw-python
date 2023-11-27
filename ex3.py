abfe19d794d752b7622fda26882e53a3f4fc4a8d
vxia444 <148790158+vxia444@users.noreply.github.com>
2023-11-27 23:25:23 +0200

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -8,9 +8,7 @@ for _ in range(n):
     
     set1 = {x for x in range(int(n1[0]), int(n1[1])+1)}
     set2 = {x for x in range(int(n2[0]), int(n2[1])+1)}
-
-    common1 = set1.intersection(set2)
-    
+    common1 = set1.intersection(set2) 
     if len(common1) > len(max_set):
         max_set = common1
 
